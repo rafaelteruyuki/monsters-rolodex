a4f0dd864242be376b82238d75e8a7a74fa3a0cb
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2019-10-13 16:54:55 -0300

    Updates

---
+++ precache-manifest.211f7a01fec1ccb96e9516a444703df3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648da065e73d8d6e2ec68c3083db1d02",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dae268a3c6fb25fb4ab4",
+    "url": "/monsters-rolodex/static/css/main.ae7e8a03.chunk.css"
+  },
+  {
+    "revision": "ca903edc950d52328219",
+    "url": "/monsters-rolodex/static/js/2.e124cbd2.chunk.js"
+  },
+  {
+    "revision": "dae268a3c6fb25fb4ab4",
+    "url": "/monsters-rolodex/static/js/main.1b010e5c.chunk.js"
+  },
+  {
+    "revision": "d71975295fe45a6cbab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.4686dfee.js"
+  }
+]);
